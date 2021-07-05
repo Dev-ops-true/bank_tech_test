e26664ba1b497625c3adeee44b664a8f552b6b8c
Dewald Viljoen <79870549+Dev-ops-true@users.noreply.github.com>
2021-07-05 11:52:30 +0100

    second test written & passed

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,10 +1,15 @@
 require 'account'
 
 describe "account" do 
+
   acc = Account.new
-  it 'creates a new account' do 
-   
-    expect(acc).to be_a(Account)
-  end
+
+    it 'creates a new account' do 
+      expect(acc).to be_a(Account)
+    end
+
+    it 'initializes a new account with a balance of 0' do 
+      expect(acc.balance).to eq(0)
+    end
   
 end
